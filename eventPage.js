d222eaab1481e12a7c177c8e30c615a298b56142
Jonathan Balls <j.balls@btopenworld.com>
2015-10-07 23:28:34 +0100

    Persistent checking for failing services and a badge to display number which are currently failing

--- eventPage.js
+++ eventPage.js
@@ -13,3 +13,46 @@ chrome.runtime.onMessage.addListener(
     
     return true;
 });
+
+// Continuously check for failing services
+window.setInterval(function(){
+
+    chrome.storage.sync.get({
+    serverUrl: ''
+    }, function(items) {
+            server_url = items.serverUrl;
+            chrome.cookies.get({ url: server_url, name: 'csrftoken' },
+            function (cookie) {
+            if (cookie) {
+
+                $.ajax({
+                    url: server_url + '/api/services/',
+                    dataType: "json",
+                    data: {
+                      'csrfmiddlewaretoken': cookie.value
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.log(jqXHR);
+                        return;
+                    },
+                    success: function(data, textStatus, jqXHR) {
+                        var failing = 0;
+
+                        var i;
+                        for (i=0; i<data.length; i++) {
+                            if (data[i].overall_status != "PASSING") {
+                                failing = failing + 1;
+                            }
+                            console.log(data[i]);
+                        }
+                        chrome.browserAction.setBadgeText({text: failing.toString()});
+                    }
+                });
+
+
+            } else {
+                return;
+            }
+        });
+    });
+}, 1000);
